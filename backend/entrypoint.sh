e48f555430234eea7dc6259a6afe3753c1b089f9
mfernandezl <mfernandezl@fi.uba.ar>
2025-07-05 23:46:00 -0300

    Rollback

--- backend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Esperar a que mariadb esté listo
-/wait-for-it.sh mariadb:3306 --timeout=60 --strict -- echo "MariaDB is up"
-
-# Ejecutar la aplicación
-exec java -jar /home/app/product-example-0.0.1-SNAPSHOT.jar
-
-
